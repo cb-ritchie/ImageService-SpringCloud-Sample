f743062d34a641a41e10bbf78d4395655456c838
cb-Ritchie <ritchie@chargebee.com>
2019-12-19 11:56:34 +0530

    Sample Image Service Completed

---
+++ src/main/java/com/chargebee/gw/demo/imageservice/model/Image.java
@@ -0,0 +1,38 @@
+package com.chargebee.gw.demo.imageservice.model;
+
+public class Image {
+
+    private int id;
+    private String title;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Image(int id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+}
